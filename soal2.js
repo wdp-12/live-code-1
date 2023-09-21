a3bb3b9ca161464dccfa7207844e59837f5b099d
RafhiSaid <mrafhisaidhafiz@gmail.com>
2023-09-21 20:45:01 +0700

    soal 2

---
+++ soal2.js
@@ -0,0 +1,25 @@
+function getEvenNumbers(num) {
+    // let numbers = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    let even = [];
+    for(let i = 1; i < numbers.length; i++) {
+       if (i % 2 == 0)
+       getResult.push(i)    
+    }
+    return result;
+}
+
+console.log(getEvenNumbers(16));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16 ];
+
+console.log(getEvenNumbers(17));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16 ];
+
+console.log(getEvenNumbers(18));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16, 18 ];
+
+console.log(getEvenNumbers(1));
+// Output
+// [];
