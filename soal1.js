b9eeff52643689705c3eeaabd7f433cd60aaee83
hamdanzull <hmdan.zulfa@gmail.com>
2023-09-21 20:42:14 +0700

    first commit

--- soal1.js
+++ soal1.js
@@ -1,21 +1,21 @@
 function getResult(score) {
     if (score >= 80 && score <= 100) {
-        return "Grade A";
+        return "mendapatkan Grade A";
     } else if (score >= 60 && score <= 79) {
-        return "Grade B";
+        return "mendapatkan Grade B";
     } else if (score >= 40 && score <= 59) {
-        return "Grade C";
+        return "mendapatkan Grade C";
     } else if (score >= 20 && score <= 39) {
-        return "Grade D";
+        return "mendapatkan Grade D";
     } else if (score >= 0 && score <= 19) {
-        return "Grade E";
+        return "mendapatkan Grade E";
     } else {
         return "Nilai tidak valid";
     }
 }
 
-getResult("Adrian", 78);
-getResult("Rafhi", 80);
-getResult("Annisa", 1000);
-getResult("Taufik", 21);
-getResult("Taufik", -10);
+console.log("Adrian", getResult(78));
+console.log("Rafhi", getResult(80));
+console.log("Annisa", getResult(1000));
+console.log("Taufik", getResult(21));
+console.log("Taufik", getResult(-10));
