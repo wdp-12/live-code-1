88fc66a655aa9833519df629a77cf1791988b96c
ranggargs_11 <ranggaargs11@gmail.com>
2023-09-21 20:40:36 +0700

    Soal2

---
+++ soal2.js
@@ -0,0 +1,14 @@
+function getEvenNumbers(num) {
+    let evenNumbers = [];
+    for (let i = 2; i <= num; i += 2) {
+        evenNumbers.push(i);
+    }
+    return evenNumbers;
+}
+
+console.log(getEvenNumbers(16));
+console.log(getEvenNumbers(17));
+console.log(getEvenNumbers(18));
+console.log(getEvenNumbers(1));
+
+
