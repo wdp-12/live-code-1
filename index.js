32fa037ac8c6ae0877ea64985cbdc7f80383bb35
odiliahln <odiliahelene99@gmail.com>
2023-09-21 20:32:40 +0700

    pesan

---
+++ index.js
@@ -0,0 +1,39 @@
+function getResult(score){
+    let grade;
+    if( score < 100 || score > 80) {
+        grade = 'A'
+    }
+    return grade;
+}
+
+function getResult(score){
+    let grade;
+    if( score < 79 || score > 60) {
+        grade = 'B'
+    }
+    return grade;
+}
+
+function getResult(score){
+    let grade;
+    if( score < 59 || score > 40) {
+        grade = 'C'
+    }
+    return grade;
+}
+
+function getResult(score){
+    let grade;
+    if( score < 39 || score > 20) {
+        grade = 'D'
+    }
+    return grade;
+}
+
+function getResult(score){
+    let grade;
+    if( score < 19 || score > 0) {
+        grade = 'E'
+    }
+    return grade;
+}
