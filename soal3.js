3cebe65c9ca94256ddbc53664b843ff05bad3fb4
ivanwijdan16 <ivanok152@gmail.com>
2023-09-21 21:05:12 +0700

    answer

--- soal3.js
+++ soal3.js
@@ -1,1 +1,40 @@
-//ok
+function sortingItems(array) {
+  // tulis kode disini
+  let res = {};
+
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    let key = item[0];
+
+    if (!res[key]) {
+      res[key] = [];
+    }
+    res[key].push(item);
+  }
+
+  //sort
+let resSort = {};
+let 
+
+  return console.log(res);
+}
+
+const fruits = ["Banana", "Pomegranate", "Apple", "Pears", "Avocado", "Appricot"];
+
+sortingItems(fruits);
+// Output
+// {
+//     a: [ "Apple", "Appricot", "Avocado" ],
+//     b: [ "Banana" ],
+//     p: [ "Pears", "Pomegranate" ],
+// }
+
+const pokemons = ["Rattata", "Pikachu", "Pidgey", "Nidoran", "Raichu"];
+
+sortingItems(pokemons);
+// Output
+// {
+//     n: [ "Nidoran" ],
+//     p: [ "Pidgey", "Pikachu" ],
+//     r: [ "Raichu", "Rattata" ],
+// }
