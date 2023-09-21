1c618ab69150c9ae3def1646f11018e94d3e9273
annisam23 <annisamrahma23@gmail.com>
2023-09-21 20:32:48 +0700

    bismillah

--- 1.js
+++ 1.js
@@ -1,1 +1,28 @@
-let test = "";
+function getResult(name,score) {
+    // score 100 - 80 : "grade A"
+    // score 79 - 68 : "grade B"
+    // score 59 - 48 : "grade C"
+    // score 39 - 20 : "grade D, Belajar lagi nak"
+    // score 19 - 0  : "grade E, Belajar lagi nak"
+    // jika nilai berada di luar rentang 0 - 100 , tampilkan pesan "Nilai tidak valid"
+
+    if (score >= 80 && score <= 100) {
+        console.log(`${name} mendapatkan grade A`);
+    } else if (score >= 68 && score <= 79) {
+        console.log(`${name} mendapatkan grade B`);
+    } else if (score >= 48 && score <= 59) {
+        console.log(`${name} mendapatkan grade C`);
+    } else if (score >= 20 && score <= 39) {
+        console.log(`${name} mendapatkan grade D, Belajar Lagi nak`);
+    } else if (score >= 0 && score <= 19) {
+        console.log(`${name} mendapatkan grade E, Belajar Lagi nak`);
+    } else {
+        console.log("Nilai Tidak Valid");
+    }
+}
+
+getResult("Adrian",78); // Output: "grade B"
+getResult("Rafhi",83); // Output: "grade A"
+getResult("Annisa",1000); // Output: "Nilai Tidak Valid"
+getResult("Taufik",21); // Output: "grade D, Belajar lagi nak"
+getResult("Taufik",-18); // Output: "Nilai Tidak Valid"
