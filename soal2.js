83e00cee37f535e43c9bdbe0fd50d02abdb65f55
shehnazeyma <icyenas@gmail.com>
2023-09-21 20:44:46 +0700

    Create soal2.js

---
+++ soal2.js
@@ -0,0 +1,24 @@
+function getEvenNumbers(num) {
+    const angka = [];
+
+    for (let i = 2; angka.length <= num; i += 2) { 
+    }
+    
+    return angka;
+}
+
+console.log(getEvenNumbers(16));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16 ];
+
+console.log(getEvenNumbers(17));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16 ];
+
+console.log(getEvenNumbers(18));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16, 18 ];
+
+console.log(getEvenNumbers(1));
+// Output
+// [];
