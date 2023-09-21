b56ba8ffaf15718c4f587a53996f96f0b9320c31
Rozan-Latukau <iyanlatu@gmail.com>
2023-09-21 20:33:00 +0700

    first commit

--- soal1.js
+++ soal1.js
@@ -1,22 +1,20 @@
 function getResults(score) {
-    let nilai;
-    for (nilai = 0; nilai < score; i++) {
-        if (nilai <= 100 && nilai >= 80) {
-            console.log("Grade: A");
-        } else if (nilai <= 100 && nilai >= 80) {
-            console.log("Grade: B");
-        } else if (nilai <= 79 && nilai >= 60) {
-            console.log("Grade: C");
-        } else if (nilai <= 59 && nilai >= 40) {
-            console.log("Grade :D, belajar Lagi Nak");
-        } else if (nilai <= 10 && nilai >= 0) {
-            console.log("Grade :E, belajar lagi Nak");
-        } else {
-            console.log("Nilai Tidak valid");
-        }
-        return nilai;
-    }
+    let nilai = 80;
 
+    if (nilai <= 100 && nilai >= 80) {
+        console.log("Grade: A");
+    } else if (nilai <= 100 && nilai >= 80) {
+        console.log("Grade: B");
+    } else if (nilai <= 79 && nilai >= 60) {
+        console.log("Grade: C");
+    } else if (nilai <= 59 && nilai >= 40) {
+        console.log("Grade :D, belajar Lagi Nak");
+    } else if (nilai <= 10 && nilai >= 0) {
+        console.log("Grade :E, belajar lagi Nak");
+    } else {
+        console.log("Nilai Tidak valid");
+    }
+    return nilai;
 
 }
 
