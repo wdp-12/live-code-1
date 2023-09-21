454f9d8bfd48fee0ea09013d1ec807501e44328b
yabsiraditya <yabsir662@gmail.com>
2023-09-21 20:33:02 +0700

    soal 1

--- soal1.js
+++ soal1.js
@@ -8,7 +8,7 @@ function getResult(nama, score) {
         console.log(nama + " Grade : C"); 
     } else if (score > 20) {
         console.log(nama + " Grade : D Belajar Lagi Nak"); 
-    } else if (score < 19) {
+    } else if (score > 19) {
         console.log(nama + " Grade : E Belajar Lagi Nak"); 
     } else {
         console.log(nama + " Nilai Tidak Valid"); 
