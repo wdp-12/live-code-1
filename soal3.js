6bb709d7d093339c88c43883587874cb7a81fb07
Demitri-Ansssss <ansoricahgreges@gmail.com>
2023-09-21 21:08:03 +0700

    soal3

---
+++ soal3.js
@@ -0,0 +1,29 @@
+function sortingItems(array) {
+    // tulis kode disini
+    let tempat = []
+    for(let i = 0; i<array.length-1;i++){
+        fruits.push([])
+        console.log(tempat)
+    }
+    return tempat;
+    
+}
+
+const fruits = [ "Banana", "Pomegranate", "Apple", "Pears", "Avocado", "Appricot" ];
+
+sortingItems(fruits) 
+// Output
+// {
+//     a: [ "Apple", "Appricot", "Avocado" ],
+//     b: [ "Banana" ],
+//     p: [ "Pears", "Pomegranate" ],
+// }
+
+const pokemons = [ "Rattata", "Pikachu", "Pidgey", "Nidoran", "Raichu" ];
+
+sortingItems(pokemons) 
+// Output
+// {
+//     n: [ "Nidoran" ],
+//     p: [ "Pidgey", "Pikachu" ],
+//     r: [ "Raichu", "Rattat
