8af1a6bf5c244a8e1075bb66e8f0e9d15f04e498
RafhiSaid <mrafhisaidhafiz@gmail.com>
2023-09-21 21:33:35 +0700

    soal 4

---
+++ soal4.js
@@ -0,0 +1,47 @@
+function createBlocks(block, size) {
+    let star = '';
+    for (let i = 1; i * <= block; i++) {
+        for (let j = 1; j <= size; j++) {
+            star += '*';
+        }
+        star += '\n';
+    }
+    console.log(star);
+}
+
+createBlocks(3, 4);
+// output
+// * *
+// * *
+// * * * *
+// * * * *
+// * * * * * *
+// * * * * * *
+
+createBlocks(2, 9);
+// output
+// * * *
+// * * *
+// * * *
+// * * * * * *
+// * * * * * *
+// * * * * * *
+
+createBlocks(1, 16);
+// output
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+createBlocks(1, 13);
+// output
+// "size invalid"
+
+createBlocks(0, 4);
+// output
+// "block invalid"
+
+createBlocks(-1, 3);
+// output
+// "size invalid, block invalid"
