c8084028b9940e9868ac9ed62f0fc5fdc39ff126
Ferdinantampubolon <tampubolonferdinan9@gmail.com>
2023-09-21 20:32:49 +0700

    create soal1.js

--- soal1.js
+++ soal1.js
@@ -1,1 +1,11 @@
-test
+function getResult(score) {
+    const score = 100-80; "Grade A"
+    const score = 79-60; "Grade B"
+    const score = 59-40; "Grade C"
+    const score = 39-20; "Grade D", "Belajar Lagi Nak"
+    const score = 19-0; "Grade E", "Belajar Lagi Nak"
+
+    return score;
+}
+const hasil = getResult();
+console.log(hasil);
