be77fbbf69bca81276ee88c3881224cab7dbc27e
RafhiSaid <mrafhisaidhafiz@gmail.com>
2023-09-21 21:08:37 +0700

    soal 3

---
+++ soal3.js
@@ -0,0 +1,33 @@
+function sortingItems(array) {
+    fruits.sort();
+    if(index === 0){
+
+    }else if (index === 1){
+    
+    }else if (index === 2){
+    
+    }
+    pokemons.sort();
+
+   console.log(fruits);
+   console.log(pokemons);
+}
+
+const fruits = [ "Banana", "Pomegranate", "Apple", "Pears", "Avocado", "Appricot" ];
+
+sortingItems(fruits) 
+// Output
+// {
+//     a: [ "Apple", "Appricot", "Avocado" ],
+//     b: [ "Banana" ],
+//     p: [ "Pears", "Pomegranate" ],
+// }
+
+const pokemons = [ "Rattata", "Pikachu", "Pidgey", "Nidoran", "Raichu" ];
+
+sortingItems(pokemons) 
+// Output
+// {
+//     n: [ "Nidoran" ],
+//     p: [ "Pidgey", "Pikachu" ],
+//     r: [ "Raichu", "Rattat
