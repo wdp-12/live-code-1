991c479221ae4bb38142cf1a356e20ae2b3a2e66
rasyid sulthan <rasyidsulthan2002@gmail.com>
2023-09-21 21:02:51 +0700

    add soal3.js

---
+++ soal3.js
@@ -0,0 +1,10 @@
+function sortingItems(array){
+
+}
+const fruits = ["Banana", "Pomegranate", "Apples", "Pears", "Avocado", "Apricot"];
+
+sortingItems(fruits)
+
+const pokemons = ["Rattata", "Pikachu", "Pidgey", "Nidoran", "Raichu"]
+
+sortingItems(pokemons) 
