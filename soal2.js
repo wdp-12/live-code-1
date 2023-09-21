e82e3a46b3c6d4549c3f528d56da478d5a27330b
Rozan-Latukau <iyanlatu@gmail.com>
2023-09-21 20:44:09 +0700

    first commit

---
+++ soal2.js
@@ -0,0 +1,19 @@
+function getEvenNumber(now) {
+    let nilai;
+    for (let i = 0; i < now; i++) {
+        if (nilai * 2) {
+            console.log(nilai);
+        } else {
+
+        }
+    }
+
+    return nilai
+}
+console.log(getEvenNumber(16));
+
+console.log(getEvenNumber(17));
+
+console.log(getEvenNumber(18));
+
+console.log(getEvenNumber(1));
