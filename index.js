b65223829561dc158154b834c8376d664aed5982
odiliahln <odiliahelene99@gmail.com>
2023-09-21 20:46:06 +0700

    pesan

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-function getResult(score){
-    let grade;
-    if( score < 100 || score > 80) {
-        grade = 'A'
+// function getResult(score){
+//     let grade;
+//     if( score < 100 || score > 80) {
+//         grade = 'A'
+//     }
+//     return grade;
+// }
+
+// function getResult(score){
+//     let grade;
+//     if( score < 79 || score > 60) {
+//         grade = 'B'
+//     }
+//     return grade;
+// }
+
+// function getResult(score){
+//     let grade;
+//     if( score < 59 || score > 40) {
+//         grade = 'C'
+//     }
+//     return grade;
+// }
+
+// function getResult(score){
+//     let grade;
+//     if( score < 39 || score > 20) {
+//         grade = 'D'
+//     }
+//     return grade;
+// }
+
+// function getResult(score){
+//     let grade;
+//     if( score < 19 || score > 0) {
+//         grade = 'E'
+//     }
+//     return grade;
+// }
+
+function getEvenNumbers(num) {
+    const even_numbers = []
+    for (let i = 1; i <= num; i++){
+        if i % 2 == 0;
     }
-    return grade;
+   
+
+    return even_numbers
+
+    // tulis kode disini
 }
 
-function getResult(score){
-    let grade;
-    if( score < 79 || score > 60) {
-        grade = 'B'
-    }
-    return grade;
-}
+console.log(getEvenNumbers(16));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16 ];
 
-function getResult(score){
-    let grade;
-    if( score < 59 || score > 40) {
-        grade = 'C'
-    }
-    return grade;
-}
+console.log(getEvenNumbers(17));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16 ];
 
-function getResult(score){
-    let grade;
-    if( score < 39 || score > 20) {
-        grade = 'D'
-    }
-    return grade;
-}
+console.log(getEvenNumbers(18));
+// Output
+// [ 2, 4, 6, 8, 10, 12, 14, 16, 18 ];
 
-function getResult(score){
-    let grade;
-    if( score < 19 || score > 0) {
-        grade = 'E'
-    }
-    return grade;
-}
+console.log(getEvenNumbers(1));
+// Output
+// [];
