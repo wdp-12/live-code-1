79866b5535c73bc057e443e1bbfc605cd819999f
Hamzah Nasrulloh <nasrullohhamzah@gmail.com>
2023-09-21 21:33:31 +0700

    Update soal4.js

---
+++ soal4.js
@@ -0,0 +1,72 @@
+function createBlocks(block, size) {
+    // jika size selain 1, 4, 9, 16 tampilkan "size invalid"
+    // jika block <= 0 tampilkan "block invalid"
+    // tulis kode disini
+    if (block <= 0) {
+        if (size !== 1 && size !== 4 && size !== 9 && size !== 16) {
+          return "size invalid, block invalid";
+        } else {
+          return "block invalid";
+        }
+      }
+    
+      if (size !== 1 && size !== 4 && size !== 9 && size !== 16) {
+        return "size invalid";
+      }
+    
+      let result = "";
+    
+      for (let i = 1; i <= block; i++) {
+        for (let j = 1; j <= size; j++) {
+          result += "* ";
+        }
+        result += "\n";
+      }
+    
+      return result;
+}
+
+createBlocks(3, 4);
+// output
+// * *
+// * *
+// * * * *
+// * * * *
+// * * * * * *
+// * * * * * *
+
+createBlocks(2, 9);
+// output
+// * * *
+// * * *
+// * * *
+// * * * * * *
+// * * * * * *
+// * * * * * *
+
+createBlocks(1, 16);
+// output
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+createBlocks(1, 13);
+// output
+// "size invalid"
+
+createBlocks(0, 4);
+// output
+// "block invalid"
+
+createBlocks(-1, 3);
+// output
+// "size invalid, block invalid"
+
+
+console.log(createBlocks(3, 4));
+console.log(createBlocks(2, 9));
+console.log(createBlocks(1, 16));
+console.log(createBlocks(1, 13));
+console.log(createBlocks(0, 4));
+console.log(createBlocks(-1, 3));
