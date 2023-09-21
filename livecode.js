97e0bd834ae1aa61ca6e177913af5533f2544363
Jihan Azizah <143238138+j1hann@users.noreply.github.com>
2023-09-21 20:33:47 +0700

    Add files via upload

---
+++ livecode.js
@@ -0,0 +1,10 @@
+function getResult(score) {
+    let score = Number;
+    const nama = "";
+    if (score >= 80 && score <=100) {
+        let nilai = "Grade A"
+        console.log(nama + "mendapatkan" + nilai)
+    }
+}
+
+getResult(85);
