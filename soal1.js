cd296f485b66ddcca68ee4e5ab2ecf45d7e665cb
Rozan-Latukau <iyanlatu@gmail.com>
2023-09-26 22:24:35 +0700

    soal 1

--- soal1.js
+++ soal1.js
@@ -1,24 +1,24 @@
 function getResults(score) {
-    let nilai = 80;
-
+    let nilai = hasil;
+    console.log(nilai);
     if (nilai <= 100 && nilai >= 80) {
-        console.log("Grade: A");
-    } else if (nilai <= 100 && nilai >= 80) {
-        console.log("Grade: B");
+        console.log(score + " Grade: A");
     } else if (nilai <= 79 && nilai >= 60) {
-        console.log("Grade: C");
+        console.log(score + " Grade: B");
     } else if (nilai <= 59 && nilai >= 40) {
-        console.log("Grade :D, belajar Lagi Nak");
-    } else if (nilai <= 10 && nilai >= 0) {
-        console.log("Grade :E, belajar lagi Nak");
+        console.log(score + " Grade: C");
+    } else if (nilai <= 39 && nilai >= 20) {
+        console.log(score + " Grade : D, belajar Lagi Nak");
+    } else if (nilai <= 19 && nilai >= 0) {
+        console.log(score + " Grade : E, belajar lagi Nak");
     } else {
         console.log("Nilai Tidak valid");
     }
     return nilai;
 }
 
-getResults("Rozan", 78);
-getResults("Fathin", 80);
-getResults("Angga", 1000);
-getResults("Yanuar", 21);
-getResults("Wawan", 10);
+getResults("Rozan", hasil = 78);
+getResults("Fathin", hasil = 80);
+getResults("Angga", hasil = 1000);
+getResults("Yanuar", hasil = 21);
+getResults("Wawan", hasil = -10);
